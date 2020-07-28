1afd46abcea3799885d619a9bd720ccf5037ab04
Maksim Vologin <vologinmp@gmail.com>
2020-07-28 15:53:30 +0300

    Updated with my project solutions. On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Week3/Project/PySparkJob.py
    	new file:   Week3/Project/PySparkJob.py.old
    	modified:   Week5/SparkML/Project/PySparkMLFit.py
    	new file:   Week5/SparkML/Project/PySparkMLFit.py.old
    	modified:   Week5/SparkML/Project/PySparkMLPredict.py
    	new file:   Week5/SparkML/Project/PySparkMLPredict.py.old

--- Week3/Project/PySparkJob.py
+++ Week3/Project/PySparkJob.py
@@ -4,10 +4,22 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, datediff
 from pyspark.sql import functions as F
 
+def writer(df, target_path):
+    train, test, validate = df.randomSplit([0.5, 0.25, 0.25])
+    train.write.parquet('%s/'%target_path+'train/')
+    test.write.parquet('%s/'%target_path+'test/')
+    validate.write.parquet('%s/'%target_path+'validate/')
 
 def process(spark, input_file, target_path):
-    # TODO Ваш код
-
+    df = spark.read.parquet(input_file)
+    cnt_cond = lambda cond: F.sum(F.when(cond, 1).otherwise(0))
+    df = df.withColumn('is_cpm', F.when(col('ad_cost_type')=='CPM', 1).otherwise(0)).withColumn('is_cpc', F.when(col('ad_cost_type')=='CPC', 1).otherwise(0))\
+        .drop('ad_cost_type','client_union_id','compaign_union_id','platform')\
+        .groupBy('ad_id','target_audience_count','has_video','is_cpm','is_cpc','ad_cost').agg(
+            (datediff(F.max('date'),F.min('date'))+1).alias('day_count'),
+            (cnt_cond(F.col('event') == 'click')/cnt_cond(F.col('event') == 'view')).alias('CTR')
+        )
+    writer(df,target_path)
 
 def main(argv):
     input_path = argv[0]
--- Week5/SparkML/Project/PySparkMLFit.py
+++ Week5/SparkML/Project/PySparkMLFit.py
@@ -4,18 +4,27 @@ import sys
 from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.regression import LinearRegression
+from pyspark.ml.regression import RandomForestRegressor
 from pyspark.sql import SparkSession
+from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
 
-# Используйте как путь куда сохранить модель
 MODEL_PATH = 'spark_ml_model'
 
-
 def process(spark, train_data, test_data):
-    #train_data - путь к файлу с данными для обучения модели
-    #test_data - путь к файлу с данными для оценки качества модели
-    #Ваш код
-
+    df = spark.read.parquet(train_data)
+    assembler = VectorAssembler(inputCols=df.columns[:-1], outputCol='features')
+    rf = RandomForestRegressor(labelCol='ctr')
+    pipeline = Pipeline(stages=[assembler, rf])
+    evaluator = RegressionEvaluator(labelCol='ctr')
+    grid = ParamGridBuilder()\
+        .addGrid(rf.maxDepth, [3, 5, 7, 10])\
+        .addGrid(rf.numTrees, [3, 6, 9, 12, 15, 18, 21, 24])\
+        .addGrid(rf.subsamplingRate, [0.8, 0.9, 1])\
+        .build()
+    tvs = TrainValidationSplit(estimator=pipeline, evaluator=evaluator, estimatorParamMaps=grid, trainRatio=0.8)
+    model = tvs.fit(df)
+    model.bestModel.save(MODEL_PATH)
+    print(evaluator.evaluate(model.transform(spark.read.parquet(test_data))))
 
 def main(argv):
     train_data = argv[0]
@@ -25,10 +34,8 @@ def main(argv):
     spark = _spark_session()
     process(spark, train_data, test_data)
 
-
 def _spark_session():
     return SparkSession.builder.appName('PySparkMLFitJob').getOrCreate()
-
 
 if __name__ == "__main__":
     arg = sys.argv[1:]
